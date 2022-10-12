6a675d865d035f6b1ddbde0d4828e712b203929d
Alberto Carollo <baba75@gmail.com>
2022-10-12 10:54:37 +0200

    switch to 14.0

--- __manifest__.py
+++ __manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Specific Industry Applications',
-    'version': '13.0',
+    'version': '14.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base','product','web_tree_image_tooltip'],
